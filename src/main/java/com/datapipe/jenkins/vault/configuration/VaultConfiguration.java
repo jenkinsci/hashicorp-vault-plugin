b621c052a24aa49c8cae0f15b35003944577ecbd
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-04-07 21:55:53 +0200

    Handle null when normalizing URLs

--- src/main/java/com/datapipe/jenkins/vault/configuration/VaultConfiguration.java
+++ src/main/java/com/datapipe/jenkins/vault/configuration/VaultConfiguration.java
@@ -1,29 +1,31 @@
 package com.datapipe.jenkins.vault.configuration;
 
-import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
-import com.cloudbees.plugins.credentials.domains.DomainRequirement;
-import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
-import com.datapipe.jenkins.vault.credentials.VaultTokenCredential;
-import hudson.Extension;
-import hudson.model.AbstractDescribableImpl;
-import hudson.model.Descriptor;
-import hudson.model.Item;
-import hudson.security.ACL;
-import hudson.util.ListBoxModel;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 
-import java.util.List;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
+import com.datapipe.jenkins.vault.credentials.VaultTokenCredential;
+
+import hudson.Extension;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
+import hudson.model.Item;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
 
 public class VaultConfiguration extends AbstractDescribableImpl<VaultConfiguration> {
     private String vaultUrl;
 
     private String vaultTokenCredentialId;
 
-    public VaultConfiguration(){
+    public VaultConfiguration() {
         // no args constructor
     }
 
@@ -33,20 +35,20 @@ public class VaultConfiguration extends AbstractDescribableImpl<VaultConfigurati
         this.vaultTokenCredentialId = vaultTokenCredentialId;
     }
 
-    public VaultConfiguration(VaultConfiguration toCopy){
+    public VaultConfiguration(VaultConfiguration toCopy) {
         this.vaultUrl = toCopy.getVaultUrl();
         this.vaultTokenCredentialId = toCopy.getVaultTokenCredentialId();
     }
 
-    public VaultConfiguration mergeWithParent(VaultConfiguration parent){
-        if (parent == null){
+    public VaultConfiguration mergeWithParent(VaultConfiguration parent) {
+        if (parent == null) {
             return this;
         }
         VaultConfiguration result = new VaultConfiguration(this);
-        if (StringUtils.isBlank(result.getVaultTokenCredentialId())){
+        if (StringUtils.isBlank(result.getVaultTokenCredentialId())) {
             result.setVaultTokenCredentialId(parent.getVaultTokenCredentialId());
         }
-        if (StringUtils.isBlank(result.getVaultUrl())){
+        if (StringUtils.isBlank(result.getVaultUrl())) {
             result.setVaultUrl(parent.getVaultUrl());
         }
         return result;
@@ -78,15 +80,21 @@ public class VaultConfiguration extends AbstractDescribableImpl<VaultConfigurati
         }
 
         public ListBoxModel doFillVaultTokenCredentialIdItems(@AncestorInPath Item item, @QueryParameter String uri) {
-            // This is needed for folders: credentials bound to a folder are realized through domain requirements
+            // This is needed for folders: credentials bound to a folder are
+            // realized through domain requirements
             List<DomainRequirement> domainRequirements = URIRequirementBuilder.fromUri(uri).build();
-            return new StandardListBoxModel().includeEmptyValue().includeAs(ACL.SYSTEM, item, VaultTokenCredential.class, domainRequirements);
+            return new StandardListBoxModel().includeEmptyValue().includeAs(ACL.SYSTEM, item,
+                    VaultTokenCredential.class, domainRequirements);
         }
     }
 
     private String normalizeUrl(String url) {
-        if(url.endsWith("/")) {
-            return url.substring(0, url.length() -1);
+        if(url == null) {
+            return null;
+        }
+
+        if (url.endsWith("/")) {
+            return url.substring(0, url.length() - 1);
         }
 
         return url;
