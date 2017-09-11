3f308266ae6775000bdf7f81ff386acd046c7cc5
Peter Tierno <ptierno@datapipe.com>
2017-09-11 17:23:15 -0400

    [JENKINS-46792] tests

--- src/test/java/com/datapipe/jenkins/vault/log/MaskingConsoleLogFilterSpec.java
+++ src/test/java/com/datapipe/jenkins/vault/log/MaskingConsoleLogFilterSpec.java
@@ -11,9 +11,11 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -66,5 +68,17 @@ public class MaskingConsoleLogFilterSpec {
 
         assertThat(resultingLines[0], is("This is a test."));
         assertThat(resultingLines[1], is("This is veryverysecret test."));
+    }
+
+    @Test
+    public void shouldFilterNullSecrets() throws Exception {
+        List<String> secrets = Arrays.asList("secret", null, "another", null, "last");
+
+        try {
+            String pattern = MaskingConsoleLogFilter.getPatternStringForSecrets(secrets);
+            assertThat(pattern, is("\\Qanother\\E|\\Qsecret\\E|\\Qlast\\E"));
+        } catch (NullPointerException npe) {
+            fail("NullPointerException thrown");
+        }
     }
 }
