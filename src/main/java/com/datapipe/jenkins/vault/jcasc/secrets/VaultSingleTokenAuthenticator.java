601882ddc3a10fceb2b94bad269b02a6ddca6e30
Joseph Petersen <josephp90@gmail.com>
2019-08-02 22:06:52 +0200

    Move JCasC Vault Secrets to Hashicorp Vault Plugin

---
+++ src/main/java/com/datapipe/jenkins/vault/jcasc/secrets/VaultSingleTokenAuthenticator.java
@@ -0,0 +1,18 @@
+package com.datapipe.jenkins.vault.jcasc.secrets;
+
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+
+public class VaultSingleTokenAuthenticator implements VaultAuthenticator {
+    private String token;
+
+    public VaultSingleTokenAuthenticator(String token) {
+        this.token = token;
+    }
+
+    public void authenticate(Vault vault, VaultConfig config) throws VaultException {
+        // No special mechanism - token already exists
+        config.token(token).build();
+    }
+}
