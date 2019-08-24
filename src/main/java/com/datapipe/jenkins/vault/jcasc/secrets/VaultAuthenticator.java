77e30ef35906ee49cbd70cfef506a53ce106594a
Joseph Petersen <josephp90@gmail.com>
2019-08-24 02:51:30 +0200

    Add vault secret source for JCasC (#35)
    * Move JCasC Vault Secrets to Hashicorp Vault Plugin
    
    * Deprecate CASC_VAULT_PATH
    
    * add casz to developer
    
    * update to jcasc 1.28

---
+++ src/main/java/com/datapipe/jenkins/vault/jcasc/secrets/VaultAuthenticator.java
@@ -0,0 +1,18 @@
+package com.datapipe.jenkins.vault.jcasc.secrets;
+
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+
+public interface VaultAuthenticator {
+    void authenticate(Vault vault, VaultConfig config) throws VaultException;
+    static VaultAuthenticator of(String token) {
+        return new VaultSingleTokenAuthenticator(token);
+    }
+    static VaultAuthenticator of(String approle, String approleSecret) {
+        return new VaultAppRoleAuthenticator(approle, approleSecret);
+    }
+    static VaultAuthenticator of(String user, String pass, String mouthPath) {
+        return new VaultUserPassAuthenticator(user, pass, mouthPath);
+    }
+}
