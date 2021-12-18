4dfeb08ff83afc858616661365e25f16ea86a6cf
Joseph Petersen <josephp90@gmail.com>
2021-12-18 16:31:52 +0100

    formatting

--- src/main/java/com/datapipe/jenkins/vault/credentials/common/VaultHelper.java
+++ src/main/java/com/datapipe/jenkins/vault/credentials/common/VaultHelper.java
@@ -34,14 +34,16 @@ public class VaultHelper {
 
     private static final Logger LOGGER = Logger.getLogger(VaultHelper.class.getName());
 
-    static Map<String, String> getVaultSecret(@NonNull String secretPath,
-                                              @CheckForNull String prefixPath,
-                                              @CheckForNull String namespace,
-                                              @CheckForNull Integer engineVersion,
-                                              @NonNull ItemGroup<?> context) {
+    static Map<String, String> getVaultSecret(
+        @NonNull String secretPath,
+        @CheckForNull String prefixPath,
+        @CheckForNull String namespace,
+        @CheckForNull Integer engineVersion,
+        @NonNull ItemGroup<?> context) {
         try {
             Map<String, String> values;
-            SecretRetrieve retrieve = new SecretRetrieve(secretPath, prefixPath, namespace, engineVersion, context.getFullName());
+            SecretRetrieve retrieve = new SecretRetrieve(secretPath, prefixPath, namespace,
+                engineVersion, context.getFullName());
 
             Channel channel = Channel.current();
             if (channel == null) {
@@ -56,14 +58,16 @@ public class VaultHelper {
         }
     }
 
-    static String getVaultSecretKey(@NonNull String secretPath,
-                                    @NonNull String secretKey,
-                                    @CheckForNull String prefixPath,
-                                    @CheckForNull String namespace,
-                                    @CheckForNull Integer engineVersion,
-                                    @NonNull ItemGroup<? extends Item> context) {
+    static String getVaultSecretKey(
+        @NonNull String secretPath,
+        @NonNull String secretKey,
+        @CheckForNull String prefixPath,
+        @CheckForNull String namespace,
+        @CheckForNull Integer engineVersion,
+        @NonNull ItemGroup<? extends Item> context) {
         try {
-            Map<String, String> values = getVaultSecret(secretPath, prefixPath, namespace, engineVersion, context);
+            Map<String, String> values = getVaultSecret(secretPath, prefixPath, namespace,
+                engineVersion, context);
 
             if (!values.containsKey(secretKey)) {
                 String message = String.format(
@@ -78,7 +82,8 @@ public class VaultHelper {
         }
     }
 
-    private static class SecretRetrieve extends SlaveToMasterCallable<Map<String, String>, IOException> {
+    private static class SecretRetrieve extends
+        SlaveToMasterCallable<Map<String, String>, IOException> {
 
         private static final long serialVersionUID = 1L;
 
@@ -92,7 +97,12 @@ public class VaultHelper {
         @CheckForNull
         private final String credentialContextAsString;
 
-        SecretRetrieve(String secretPath, String prefixPath, String namespace, Integer engineVersion, String credentialContext) {
+        SecretRetrieve(
+            String secretPath,
+            String prefixPath,
+            String namespace,
+            Integer engineVersion,
+            String credentialContext) {
             this.secretPath = secretPath;
             this.prefixPath = Util.fixEmptyAndTrim(prefixPath);
             this.namespace = Util.fixEmptyAndTrim(namespace);
@@ -107,7 +117,6 @@ public class VaultHelper {
             }
 
             ItemGroup<? extends Item> credentialContext = getItemGroup(credentialContextAsString);
-
             if (credentialContext == null) {
                 throw new IllegalStateException("The vault credential need a context");
             }
@@ -148,17 +157,20 @@ public class VaultHelper {
                 }
 
                 VaultCredential vaultCredential = configuration.getVaultCredential();
-                if (vaultCredential == null) vaultCredential = retrieveVaultCredentials(configuration.getVaultCredentialId(),
-                    credentialContext);
+                if (vaultCredential == null) {
+                    vaultCredential = retrieveVaultCredentials(configuration.getVaultCredentialId(),
+                        credentialContext);
+                }
 
                 VaultAccessor vaultAccessor = new VaultAccessor(vaultConfig, vaultCredential);
                 vaultAccessor.setMaxRetries(configuration.getMaxRetries());
-                vaultAccessor.setRetryIntervalMilliseconds(configuration.getRetryIntervalMilliseconds());
+                vaultAccessor.setRetryIntervalMilliseconds(
+                    configuration.getRetryIntervalMilliseconds());
                 vaultAccessor.init();
 
                 return vaultAccessor.read(secretPath, engineVersion).getData();
             } catch (VaultPluginException vpe) {
-              throw vpe;
+                throw vpe;
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -179,8 +191,9 @@ public class VaultHelper {
         return item instanceof ItemGroup<?> ? (ItemGroup<?>) item : null;
     }
 
-
-    private static VaultCredential retrieveVaultCredentials(String id, ItemGroup<? extends Item> itemGroup) {
+    private static VaultCredential retrieveVaultCredentials(
+        String id,
+        ItemGroup<? extends Item> itemGroup) {
         if (StringUtils.isBlank(id)) {
             throw new VaultPluginException(
                 "The credential id was not configured - please specify the credentials to use.");
@@ -191,7 +204,7 @@ public class VaultHelper {
             .lookupCredentials(VaultCredential.class,
                 itemGroup,
                 ACL.SYSTEM,
-                Collections.<DomainRequirement>emptyList());
+                Collections.emptyList());
         VaultCredential credential = CredentialsMatchers
             .firstOrNull(credentials, new IdMatcher(id));
 
