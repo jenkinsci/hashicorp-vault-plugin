b65135394a0976e2f38354056aba64068a942c91
Jesse Glick <jglick@cloudbees.com>
2023-09-05 16:20:41 -0400

    Protect against possible race condition

--- src/main/java/com/datapipe/jenkins/vault/log/MaskingConsoleLogFilter.java
+++ src/main/java/com/datapipe/jenkins/vault/log/MaskingConsoleLogFilter.java
@@ -30,20 +30,19 @@ public class MaskingConsoleLogFilter extends ConsoleLogFilter
         updatePattern();
     }
 
-    private synchronized void updatePattern() {
+    private synchronized Pattern updatePattern() {
         if (!valuesToMask.equals(valuesToMaskInUse)) {
             List<String> values = valuesToMask.stream().filter(Objects::nonNull).collect(Collectors.toList());
             pattern = values.isEmpty() ? null : SecretPatterns.getAggregateSecretPattern(values);
             valuesToMaskInUse = new ArrayList<>(valuesToMask);
         }
+        return pattern;
     }
 
     @Override
     public OutputStream decorateLogger(Run run,
         final OutputStream logger) throws IOException, InterruptedException {
         return new SecretPatterns.MaskingOutputStream(logger,
-            () -> {
-                updatePattern();
                 // Only return a non-null pattern once there are secrets to mask. When a non-null
                 // pattern is returned it is cached and not supplied again. In cases like
                 // VaultBuildWrapper the secrets are added to the "valuesToMasker" list AFTER
@@ -51,8 +50,7 @@ public class MaskingConsoleLogFilter extends ConsoleLogFilter
                 // the Pattern should only be returned once the secrets have been made available.
                 // Not to mention it is also a slight optimization when there is are no secrets
                 // to mask.
-                return pattern;
-            },
+                this::updatePattern,
             charsetName);
     }
 
