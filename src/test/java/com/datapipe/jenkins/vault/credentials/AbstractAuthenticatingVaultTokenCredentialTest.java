3988e6598a1ba25f14e34cba6e2a921d9a9ecd50
ARUS2023 <145485937+ARUS2023@users.noreply.github.com>
2023-10-05 11:11:59 -0500

    Update AbstractAuthenticatingVaultTokenCredentialTest.java
    Removed unused imports

--- src/test/java/com/datapipe/jenkins/vault/credentials/AbstractAuthenticatingVaultTokenCredentialTest.java
+++ src/test/java/com/datapipe/jenkins/vault/credentials/AbstractAuthenticatingVaultTokenCredentialTest.java
@@ -10,10 +10,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
